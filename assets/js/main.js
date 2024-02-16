3632b543641f86884bd503e78cf94001b268aad8
Nico-Serra <nico.serra000@gmail.com>
2024-02-16 15:07:55 +0100

    add exercise 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 /*
 Esercizio 1
 Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+number = 100; // number
 */
 
 const number = 100;
@@ -12,6 +13,7 @@ console.log(number);
 /*
 Esercizio 2
 Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+PI = 3.14; // number
 */
 
 const PI = 3.14;
@@ -34,9 +36,12 @@ Esercizio 2/C
 Dichiara una variabile chiamata radius e assegnale il valore 8.
 Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
 Stampa il valore della variabile circle in console.
+
+radius = 8 ; // number
+radius (updated) = 8*2 ;// number
 */
 
-let radius = 8
+let radius = 8;
 
 console.log(radius);
 
@@ -44,9 +49,28 @@ radius = 8*2;
 
 console.log(radius);
 
+/////////////////////////////////////////////////////////////
 
-
-const circle = (radius * PI)
+const circle = (radius * PI);
 
 console.log(circle);
+
+/*
+Esercizio 3
+Dichiara una variabile chiamata name e
+assegnale il valore "Carlo".
+Successivamente, modifica il valore della variabile in "Marco".
+Stampa il nuovo valore in console.
+
+name = Carlo; // string
+name (update) = Marco; // string
+*/
+
+let name = ('Carlo')
+
+console.log(name);
+
+name = ('Marco')
+
+console.log(name);
 
