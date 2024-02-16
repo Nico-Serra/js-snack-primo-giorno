89a1288f1aab9066c549e5883344a53e70862c3a
Nico-Serra <nico.serra000@gmail.com>
2024-02-16 14:52:16 +0100

    add exercise 2 and 2B

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,23 @@ Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valo
 const number = 100;
 
 console.log(number);
+
+/*
+Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+*/
+
+const PI = 3.14;
+
+console.log(PI);
+
+/*
+Esercizio 2/B
+Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+    -Per poter dare il valore 3.1416 dovrei cambiare la mia variabile anzichè utilizzare (const) utilizzerei la variabile (let), poichè la variabile (const) non è riassegnabile e mi darebbe l'errore "Uncaught SyntaxError: Identifier 'PI' has already been declared"
+
+*/
+
+/*const PI = 3.1416;
+
+console.log(PI);*/
